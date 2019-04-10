c60876fd8acd32927473d5cb14410070b6ee730a
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-04-10 15:01:35 -0500

    Update add these.py

--- PythonAI-Linux/add these.py
+++ PythonAI-Linux/add these.py
@@ -5,3 +5,11 @@ async def pwr():
         await bot.logout()
         await bot.close()
         bot.aiosession.close()
+
+#Clear Inbetween
+@bot.command()
+@commands.has_permissions(manage_messages=True)
+async def clrin(ctx, int1: int, Int2: int):
+        clrh = min(int1, int2)
+        clrl = max(int1, int2)
+        await ctx.channel.purge(limit=2000, before=clrl, after=clrh)
