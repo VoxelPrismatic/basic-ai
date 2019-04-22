15f5a702b12f3b4fc8042b4721fb5a7408b64a26
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-04-22 14:35:04 -0500

    Update add these.py

--- PythonAI-Linux/add these.py
+++ PythonAI-Linux/add these.py
@@ -1,13 +1,10 @@
-##/// GRAPHING
-import numpy as np
-import ast
-import io
-from ast import literal_eval
-import matplotlib.pyplot as plt
+##/// REACT
 @bot.command()
-async def graph(ctx, eq, xmin, xmax):
-    x = np.array(range(xmin, xmax))  
-    y = ast.safe_eval(eq.replace("^", "**"))
-    plt.plot(x, y)
-    tempimg = io.bytesio(plt.show())
-    await ctx.send(file=discord.File(tempimg))
+async def react(ctx, mID: int, *reactions):
+    message = await ctx.fetch_message(mID)
+    for reaction in reactions:
+        try:
+            await message.add_reaction(reaction)
+        except discord.HTTPException: await exc(ctx, 1)
+        except discord.Forbidden: await exc(ctx, 2)
+        except discord.NotFound: await exc(ctx, 3)
