1a3853a962d15fab1667ef341fd87c19d2310695
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-04-22 15:39:38 -0500

    Update add these.py

--- PythonAI-Linux/add these.py
+++ PythonAI-Linux/add these.py
@@ -8,3 +8,19 @@ async def react(ctx, mID: int, *reactions):
         except discord.HTTPException: await exc(ctx, 1)
         except discord.Forbidden: await exc(ctx, 2)
         except discord.NotFound: await exc(ctx, 3)
+##/// STATS
+import statistics
+@bot.command()
+async def stats(ctx, *data: int):
+    await ctx.send(embed=embedify(f'''```md
+#] STATS
+>   MAX // {max(data)}
+>   MIN // {min(data)}
+>   AVG // {mean(data)}
+>   MOD // {mode(data)}
+>   MED // {median(data)}
+> RANGE // {max(data)-min(data)}
+> STDEV // {stdev(data)}
+> LOMED // {median_low(data)}
+> HIMED // {median_high(data)}
+```'''))
