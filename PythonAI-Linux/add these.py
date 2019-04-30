69cbf3e5aecd175576e86ebe016ae2a624254705
VoxelPrismatic <45671764+VoxelPrismatic@users.noreply.github.com>
2019-04-30 14:32:40 -0500

    Update add these.py

--- PythonAI-Linux/add these.py
+++ PythonAI-Linux/add these.py
@@ -1,68 +1,109 @@
-##/// REACT
-@bot.command()
-async def react(ctx, mID: int, *reactions):
-    message = await ctx.fetch_message(mID)
-    for reaction in reactions:
-        try:
-            await message.add_reaction(reaction)
-        except discord.HTTPException: await exc(ctx, 1)
-        except discord.Forbidden: await exc(ctx, 2)
-        except discord.NotFound: await exc(ctx, 3)
+##/// MATH MODULE
+@commands.command()
+async def lawsin(typ: int, var1: decimal, var2: decimal, var3: decimal)
+    
+    
+##/// HELP COMMAND UPDATE
+left = '⏪'
+right = '⏩'
+def predicate(message, l, r):
+    def check(reaction, user):
+        if reaction.message.id != message.id or user == bot.user:
+            return False
+        if l and reaction.emoji == left: return True
+        if r and reaction.emoji == right: return True
+        return False
+    return check
 
-##/// STATS
-import statistics
-@bot.command()
-async def stats(ctx, *data: int):
-    await ctx.send(embed=embedify(f'''```md
-#] STATS
->   MAX // {max(data)}
->   MIN // {min(data)}
->   AVG // {mean(data)}
->   MOD // {mode(data)}
->   MED // {median(data)}
-> RANGE // {max(data)-min(data)}
-> STDEV // {stdev(data)}
-> LOMED // {median_low(data)}
-> HIMED // {median_high(data)}
+@bot.command(pass_context=True)
+async def help(ctx):
+    result = 0
+    def check(reaction, user): return user == ctx.author
+    lit = ["""
+] ";]hlep"
+> Brings up this message :)
+] ";]sys"
+> Shows some sys info :P
+] ";]ping"
+> Shows the ping time :L
+] ";]slots"
+> Slot machine :D
+] ";]coin {x}" [Option: {x}]
+> Flips a virtual coin {x} times 0.0""",
+                """] ";]git"
+> Shows the github/gitlab repo ;]
+] ";]rng {x} {y} {z}" [Option: {z}]
+> Prints an RNG from {x} to {y}, {z} times
+] ";]usrinfo"
+> Shows your user info 0.0
+] ";]dnd"
+> Roles all dice from Dragons and Dungeons!
+] ";]info"
+> Shows additional info""",
+                """] ";]cool {uID}"
+> Gives someone [{userID}] a sneaky surpise ;D
+] ";]rick"
+> Rick Roll! °ω°
+] ";]blkjck"
+> BLACKJACK! 0o0
+] ";]spam {x}"
+> Spams {x} amount of chars >.<
+] ";]graph {eq} {xmin} {xmax}"
+> Graphs {eq} from {xmin} to {xmax} 9.6""",
+                """] ";]rto {x} {y}"
+> Reduces the ratio of {x} and {y} ;-;
+] ";]rad {x}"
+> Reduces a radical, {x}! >:D
+] ";]react {mID} {reactions}" [{reaction}.split " "]
+> Adds {reactions} to a given {mID} .-.
+] ";]stats {data}"
+> Gives stats given {data} ._.
+] ";]quad {a} {b} {c}"
+> Uses {a}, {b}, and {c} to solve the Quad Formula 0o0"""]
+    msg = await ctx.send(embed=embedify(f'''```md
+#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+{lit[result]}`````md
+#] To see mod commands, use ";]hlepmod"
+#] To have a conversation, use "]{your text here}""
+#] Some of your data is stored, use ";]data" to see more
 ```'''))
-
-##/// UPDATED BLACKJACK
-@bot.command()
-async def blkjck(ctx):
-    heart = ['🂱','🂲','🂳','🂴','🂵','🂶','🂷','🂸','🂹','🂺','🂻','🂽','🂾']
-    spade = ['🂡','🂢','🂣','🂤','🂥','🂦','🂧','🂨','🂩','🂪','🂫','🂭','🂮']
-    diam = ['🃁','🃂','🃃','🃄','🃅','🃆','🃇','🃈','🃉','🃊','🃋','🃍','🃎']
-    club = ['🃑','🃒','🃓','🃔','🃕','🃖','🃗','🃘','🃙','🃚','🃛','🃝','🃞']
-    ttl1 = ttl2 = temp = stu = 0
-    usr = cpu = cputxt = usrtxt = ""
-    while ttl1 <= 18:
-        temp = rand(0,12)
-        if temp + ttl1 + 1 != 21:
-            stu = rand(0,3)
-            if stu == 1: usr = usr+heart.pop[temp]+' '
-            elif stu == 2: usr = usr+spade.pop[temp]+' '
-            elif stu == 3: usr = usr+diam.pop[temp]+' '
-            elif stu == 3: usr = usr+club.pop[temp]+' '
-            temp += 1
-            if temp > 10: temp = 10
-            ttl1 += temp
-    while ttl2 <= 18:
-        temp = rand(0,12)
-        if temp + ttl2 + 1 != 21:
-            if stu == 1: cpu = cpu+heart.pop[temp]+' '
-            elif stu == 2: cpu = cpu+spade.pop[temp]+' '
-            elif stu == 3: cpu = cpu+diam.pop[temp]+' '
-            elif stu == 3: cpu = cpu+club.pop[temp]+' '
-            temp += 1
-            if temp > 10: temp = 10
-            ttl2 += temp
-    if ttl1 > 21: usrtxt = f'USER // {usr} [{ttl1}] [BUST]'
-    elif ttl1 > ttl2: usrtxt = f'USER // {usr} [{ttl1}] [WIN]'
-    elif ttl1 < ttl2: usrtxt = f'USER // {usr} [{ttl1}] [LOSS]'
-    if ttl2 > 21: cputxt = f'COMP // {cpu} [{ttl2}] [BUST]'
-    elif ttl2 > ttl1: cputxt = f'COMP // {cpu} [{ttl2}] [WIN]'
-    elif ttl2 < ttl1: cputxt = f'COMP // {cpu} [{ttl2}] [LOSS]'
-    if ttl1 == ttl2:
-        usrtxt = f'USER // {usr} [{ttl1}] [TIE]'
-        cputxt = f'COMP // {cpu} [{ttl2}] [TIE]'
-    await ctx.send(f'```md\n#]BLACK JACK!\n \n{usrtxt}\n \n{cputxt}```')
+    await msg.add_reaction('⏪')
+    await msg.add_reaction('◀')
+    await msg.add_reaction('⏹')
+    await msg.add_reaction('▶')
+    await msg.add_reaction('⏩')
+    while True:
+        try:
+            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+        except asyncio.TimeoutError: return await msg.clear_reactions()
+        else:
+            if str(reaction.emoji) == '⏪':
+                result = 0
+                await msg.remove_reaction('⏪', ctx.author)
+                await msg.edit(embed=embedify(f'''```md
+#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+{lit[result]}`````md
+#] To see mod commands, use ";]hlepmod"
+#] To have a conversation, use "]{your text here}"
+#] Some of your data is stored, use ";]data" to see more
+```'''))
+            elif str(reaction.emoji) == '◀':
+                await msg.remove_reaction('◀', ctx.author)
+                if result == 0: return
+                else: result = result - 1
+            elif str(reaction.emoji) == '⏹':
+                return await msg.clear_reactions()
+            elif str(reaction.emoji) == '▶':
+                await msg.remove_reaction('▶', ctx.author)
+                if result == (len(m.pages) - 1): return
+                else: result += 1
+            elif str(reaction.emoji) == '⏩':
+                result = len(m.pages) - 1
+                await msg.remove_reaction('⏩', ctx.author)
+            await msg.edit(embed=embedify(f'''```md
+#] !] PRIZ AI ;] [! COMMANDS LIST``````md
+{lit[result]}`````md
+#] To see mod commands, use ";]hlepmod"
+#] To have a conversation, use "]{your text here}"
+#] Some of your data is stored, use ";]data" to see more
+```'''))
